d9afe505362802d2338a97cb9c0e20690df7719b
Jason Ding <dingyue0626@gmail.com>
2020-06-21 23:46:33 -0400

    tree level order traversal

--- Week02/binary-tree-traversal.py
+++ Week02/binary-tree-traversal.py
@@ -7,9 +7,13 @@ https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 145. Binary Tree Postorder Traversal <Hard>
 https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+102. Binary Tree Level Order Traversal <Medium>
+https://leetcode.com/problems/binary-tree-level-order-traversal/
 """
 from typing import List
 from tree import BinaryTreeNode, create_binary_tree, print_binary_tree
+from collections import deque
 
 class Solution:
     def preorderTraversal1(self, root: BinaryTreeNode) -> List[int]:
@@ -32,7 +36,7 @@ class Solution:
         """
         WHITE, GREY = 0, 1
         res = []
-        stack = [(WHITE, root),]
+        stack = [(WHITE, root)]
         while stack:
             color, node = stack.pop()
             if not node:
@@ -65,7 +69,7 @@ class Solution:
         """
         WHITE, GREY = 0, 1
         res = []
-        stack = [(WHITE, root),]
+        stack = [(WHITE, root)]
         while stack:
             color, node = stack.pop()
             if not node:
@@ -98,7 +102,7 @@ class Solution:
         """
         WHITE, GREY = 0, 1
         res = []
-        stack = [(WHITE, root),]
+        stack = [(WHITE, root)]
         while stack:
             color, node = stack.pop()
             if not node:
@@ -111,19 +115,63 @@ class Solution:
                 res.append(node.val)
         return res
 
+    def levelOrder1(self, root: BinaryTreeNode) -> List[List[int]]:
+        """
+        Solution #1: Recursively
+        """
+        def traverse(node, level):
+            if not root:
+                return
+            if len(res) == level:
+                res.append([])
+            res[level].append(node.val)
+            if node.left:
+                traverse(node.left, level+1)
+            if node.right:
+                traverse(node.right, level+1)
+
+        res = []
+        traverse(root, 0)
+        return res
+
+    def levelOrder2(self, root: BinaryTreeNode) -> List[List[int]]:
+        """
+        Solution #2: Iteratively
+        """
+        if not root:
+            return []
+        res = []
+        queue = deque([root])
+        while queue:
+            n = len(queue)
+            level = []
+            for _ in range(n):
+                root = queue.popleft()
+                level.append(root.val)
+                if root.left:
+                    queue.append(root.left)
+                if root.right:
+                    queue.append(root.right)
+            res.append(level)
+        return res
+
 t = create_binary_tree([0,1,2,3,4,5,6,7,8,9])
 print_binary_tree(t)
 
 solution = Solution()
 ans = solution.preorderTraversal1(t)
-print('Binary Tree Preorder  Traversal Recursively: ', ans)
+print('Binary Tree Preorder Traversal Recursively: ', ans)
 ans = solution.preorderTraversal2(t)
-print('Binary Tree Preorder  Traversal Iteratively: ', ans)
+print('Binary Tree Preorder Traversal Iteratively: ', ans)
 ans = solution.inorderTraversal1(t)
-print('Binary Tree Inorder   Traversal Recursively: ', ans)
+print('Binary Tree Inorder Traversal Recursively: ', ans)
 ans = solution.inorderTraversal2(t)
-print('Binary Tree Inorder   Traversal Iteratively: ', ans)
+print('Binary Tree Inorder Traversal Iteratively: ', ans)
 ans = solution.postorderTraversal1(t)
 print('Binary Tree Postorder Traversal Recursively: ', ans)
 ans = solution.postorderTraversal2(t)
 print('Binary Tree Postorder Traversal Iteratively: ', ans)
+ans = solution.levelOrder1(t)
+print('Binary Tree Level Order Traversal Recursively: ', ans)
+ans = solution.levelOrder2(t)
+print('Binary Tree Level Order Traversal Iteratively: ', ans)
--- Week02/n-ary_tree_traversal.py
+++ Week02/n-ary_tree_traversal.py
@@ -10,6 +10,7 @@ https://leetcode.com/problems/n-ary-tree-level-order-traversal/
 """
 from typing import List
 from tree import TreeNode, simple_tree
+from collections import deque
 
 class Solution:
     def preorder1(self, root: TreeNode) -> List[int]:
@@ -17,7 +18,8 @@ class Solution:
         Solution #1: Recursively
         """
         def traverse(root):
-            if not root: return
+            if not root:
+                return
             res.append(root.val)
             if root.children is not None:
                 for child in root.children:
@@ -50,7 +52,8 @@ class Solution:
         Solution #1: Recursively
         """
         def traverse(root):
-            if not root: return
+            if not root:
+                return
             if root.children is not None:
                 for child in root.children:
                     traverse(child)
@@ -78,14 +81,53 @@ class Solution:
                     stack.append(child)
         return res[::-1] # the result is reversed
 
+    def levelOrder1(self, root: TreeNode) -> List[List[int]]:
+        """
+        Solution #1: Recursively
+        """
+        def traverse(root, level):
+            if not root:
+                return
+            if len(res) == level:
+                res.append([])
+            res[level].append(root.val)
+            if root.children is not None:
+                for child in root.children:
+                    traverse(child, level + 1)
+
+        res = []
+        traverse(root, 0)
+        return res
+
+    def levelOrder2(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = deque([root])
+        while queue:
+            n = len(queue)
+            level = []
+            for _ in range(n):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.children is not None:
+                    for child in node.children:
+                        queue.append(child)
+            res.append(level)
+        return res
+
 t = simple_tree()
 
 solution = Solution()
 ans = solution.preorder1(t)
-print('N-ary Tree Preorder  Traversal Recursively: ', ans)
+print('N-ary Tree Preorder Traversal Recursively: ', ans)
 ans = solution.preorder2(t)
-print('N-ary Tree Preorder  Traversal Iteratively: ', ans)
+print('N-ary Tree Preorder Traversal Iteratively: ', ans)
 ans = solution.postorder1(t)
 print('N-ary Tree Postorder Traversal Recursively: ', ans)
 ans = solution.postorder2(t)
 print('N-ary Tree Postorder Traversal Iteratively: ', ans)
+ans = solution.levelOrder1(t)
+print('N-ary Tree Level Order Traversal Recursively: ', ans)
+ans = solution.levelOrder2(t)
+print('N-ary Tree Level Order Traversal Iteratively: ', ans)
